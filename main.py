c8a3c8454faedd6172c96c0c8ee8a4a3fe980de2
Mansur <38651406+Djakhanov@users.noreply.github.com>
2025-02-27 10:49:23 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+25/02/2025
+
+Python
+"Number finder" Game
+
+Author: Mansur Djakhanov
+
+Web sahifa: https://www.linkedin.com/in/mansur-djakhanov-a44bb3108/
+"""
+
+from funksiyalar import play
+
+play()
